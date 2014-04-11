90d23792da2ec9ca63ab7ee38fa725dfd58305e9
Edwin Gutierrez <edwin.gutierrez1991@gmail.com>
2014-04-11 17:17:54 -0500

    is this other update of culttume-part3

--- assets/js/login.js
+++ assets/js/login.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
     $("#register_form").click(function(){
       $(".social_login").hide();
       $(".user_register").show();
-      $(".header_title").text('Register');
+      $(".header_title").text('Registro');
       return false;
     });
 
@@ -24,7 +24,36 @@ $(document).ready(function(){
       $(".user_login").hide();
       $(".user_register").hide();
       $(".social_login").show();
-      $(".header_title").text('Login');
+      $(".header_title").text('Iniciar Sesión');
+      return false;
+    });
+  });
+
+  $("#register_trigger").leanModal({top : 200, overlay : 0.6, closeButton: ".modal_close" });
+
+  $(function(){
+
+    // Calling Register Form
+    $("#register_form").click(function(){
+      $(".social_login").hide();
+      $(".user_register").show();
+      $(".header_title").text('Registro');
+      return false;
+    });
+
+    // Calling Login Form
+    $("#login_form").click(function(){
+      $(".social_login").hide();
+      $(".user_login").show();
+      return false;
+    });
+
+    // Going back to Social Forms
+    $(".back_btn").click(function(){
+      $(".user_login").hide();
+      $(".user_register").hide();
+      $(".social_login").show();
+      $(".header_title").text('Iniciar Sesión');
       return false;
     });
   });

1ecce5552c07eddef795f3c682e3af8af513f26e
Edwin Gutierrez <edwin.gutierrez1991@gmail.com>
2014-04-11 19:29:42 -0500

    Last update culttume

--- assets/js/login.js
+++ assets/js/login.js
@@ -28,33 +28,4 @@ $(document).ready(function(){
       return false;
     });
   });
-
-  $("#register_trigger").leanModal({top : 200, overlay : 0.6, closeButton: ".modal_close" });
-
-  $(function(){
-
-    // Calling Register Form
-    $("#register_form").click(function(){
-      $(".social_login").hide();
-      $(".user_register").show();
-      $(".header_title").text('Registro');
-      return false;
-    });
-
-    // Calling Login Form
-    $("#login_form").click(function(){
-      $(".social_login").hide();
-      $(".user_login").show();
-      return false;
-    });
-
-    // Going back to Social Forms
-    $(".back_btn").click(function(){
-      $(".user_login").hide();
-      $(".user_register").hide();
-      $(".social_login").show();
-      $(".header_title").text('Iniciar Sesión');
-      return false;
-    });
-  });
 });  
